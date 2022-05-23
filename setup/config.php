130f01facd2d1a937c8c3625115346bf2b46f4ba
gusgvra <86165586+gusgvra@users.noreply.github.com>
2022-05-22 21:01:25 -0500

    Add files via upload

---
+++ setup/config.php
@@ -0,0 +1,14 @@
+<?php
+
+define('WEBSITE','Autos Colombia');               //  NOMBRE DEL SITIO WEB
+define('URL','http://localhost/autos-Colombia/');    // 'HOME' DE LA APP
+
+/**
+ * INFO NECESARIA PARA PARA CONECCIÓN CON LA BD
+ */
+
+define('HOST','localhost');
+define('DB','autos-Colombia');
+define('USER','root');
+define('PASSWORD','');
+define('CHARSET','utf8mb4');
